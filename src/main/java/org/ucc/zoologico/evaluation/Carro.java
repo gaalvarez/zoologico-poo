0cbbe9eb8826c9f4ade16c2e15796b64d9e9184d
gaalvarez <gaalvarez@outlook.com>
2019-11-18 10:59:15 -0500

    feat: evaluación objetos y clases

---
+++ src/main/java/org/ucc/zoologico/evaluation/Carro.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package org.ucc.zoologico.evaluation;
+
+/**
+ * @author iotec_ceo
+ *
+ */
+public class Carro {
+
+	/**
+	 * PUNTO 1: cree los atributos para un objeto carro como se describen abajo:
+	 * 
+	 * Atributos:
+	 * 
+	 * marca: cadena de texto número de puertas: 
+	 * número entero. 
+	 * color: cadena de texto
+	 * 
+	 * Por ejemplo:
+	 * 
+	 * private String marca;
+	 * 
+	 * PONGA SU CÓDIGO JUSTO DEBAJO DE ÉSTE COMENTARIO.
+	 **/
+
+	/**
+	 * PUNTO 2: cree un constructo que reciba los tres atributos que definió antes:
+	 * 
+	 * Ayuda: El constructor debe ser público 
+	 * El constructo lleva el mismo nombre de
+	 * la clase. El constructor no retorna nada. 
+	 * El constructor recibe 3 parámetros.
+	 * Dentro del constructor debe inicializar los 3 atributos definidos en el punto
+	 * 1 con los valores que vienen en los parámetros
+	 * 
+	 * PONGA SU CÓDIGO JUSTO DEBAJO DE ÉSTE COMENTARIO.
+	 **/
+	
+	/**
+	 * PUNTO 3: cree todos los métodos get para los atributos definidos en el punto 1:
+	 * 
+	 * Ayuda: por ejemplo:
+	 * 
+	 * public String getMarca() {
+	 *     return this.marca;
+	 * }
+	 * 
+	 * PONGA SU CÓDIGO JUSTO DEBAJO DE ÉSTE COMENTARIO.
+	 **/
+}
---
+++ src/main/java/org/ucc/zoologico/evaluation/ClasesObjetos.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.ucc.zoologico.evaluation;
+
+/**
+ * @author iotec_ceo
+ *
+ */
+public class ClasesObjetos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * PUNTO 4: cree un objeto con la clase Carro que está dentro de éste mismo paquete.
+		 * 
+		 * Ayuda: 
+		 * 
+		 * Recuerde que para crear un objeto debe usar el operador new y llamar al constructor de la clase.
+		 * 
+		 * Puede usar los siguiente valores para crear el carro:
+		 * 
+		 * marca: VMW
+		 * número de puertas: 4
+		 * color: gris
+		 * 
+		 * PONGA SU CÓDIGO JUSTO DEBAJO DE ÉSTE COMENTARIO.
+		 **/
+		
+		
+		
+		/**
+		 * PUNTO 5: usando el objeto que creó en el punto anterior. Imprimar todo su estado.
+		 * 
+		 * Ayuda: por ejemplo, para imprimir la marca teniendo un objeto llamadao carroPrueba:
+		 * 
+		 * System.out.println("Marca: " + carroPrueba.getMarca());
+		 * 
+		 * PONGA SU CÓDIGO JUSTO DEBAJO DE ÉSTE COMENTARIO.
+		 **/
+		System.out.println();
+	}
+
+}
